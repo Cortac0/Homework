11b3231f5c2832e06bfa4986ee0c30d9ef4fb625
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-05-26 23:35:24 +0300

    Tema 1

---
+++ Tema.py
@@ -0,0 +1,30 @@
+# Task 1
+
+name = 'Maxim'
+day = 'Friday'
+print('# task 1')
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+print()
+# task 2
+
+first = 'Max'
+last = 'im'
+print('# task 2')
+print('my name is', first + last)
+print()
+# task 3
+
+a = 3
+b = 5
+print('# task 3')
+print('The variables are: a = 3 and b = 5')
+print()
+print('Aditions =', a + b)
+print('Substraction =', a - b)
+print('Division =', a / b)
+print('Multiplication =', a * b)
+print('Exponentiation =', a * b)
+print('Modulus =', a % b)
+print('Floor division =', a // b)
+print()
+print()
