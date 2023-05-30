184d44b70785d55cada17a6259b661db061a44de
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-05-30 20:57:22 +0300

    Completat task 3

--- Tema2.py
+++ Tema2.py
@@ -1,6 +1,14 @@
 # task 2
-# 'Buna lume
-print('Buna lume')
+print('# Hello world')
+print("Hello", "world!", sep=", ", end=" ")
+print('# Astazi-este-placut')
+print("Astazi", "este", "placut", sep="-", end=".\n")
+print('# Maine*am*examene')
+print("Maine", "am", "examene", sep="*", end="!\n")
 
 # task 3
-print('Nu')
+c = '#'
+a = c * 9
+b = '''       '''
+print(f'{a}\n{c}{b}{c}\n{c}{b}{c}\n{c}{b}{c}\n{a}\n\n')
+print(f'{c}{b}{c}\n{c}{b}{c}\n{a}\n{c}{b}{c}\n{c}{b}{c}')
