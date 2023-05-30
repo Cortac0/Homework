933ab9eaf317162bf754e9cf376dd5e74f6d0f4a
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-05-30 21:30:56 +0300

    Random

---
+++ Rand.py
@@ -0,0 +1,14 @@
+import random
+
+# generarea unei lise
+lista = list(range(10))
+print(f'Lista este {lista}')
+
+# prin random.simple
+lista_random = random.sample(lista, k=3)
+print(f'Lista random este {lista_random}')
+
+# cu functia shuffle
+random.shuffle(lista)
+print(f'Lista random este {lista}')
+
