becb576623aa5af26df33e6aca35a7fc4516c65b
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-06-08 17:59:14 +0300

    task 1

--- Homework/Tema7.py
+++ Homework/Tema7.py
@@ -1,34 +1,49 @@
+# task 1
+def string(word):
+    word = word.replace(",", "").replace(".", "")
+    words = word.split()
+
+    # Count the occurrences of each word
+    dictionar = {}
+    for word in words:
+        if word in dictionar:
+            dictionar[word] += 1
+        else:
+            dictionar[word] = 1
+
+    return dictionar
+
+# Example usage
+user_word = input("Enter a string: ")
+result = string(user_word)
+print(result)
+
 # task 2
-# def calcul_total(stock, prices):
-#     total_price = 0
+def calcul_total(stock, prices):
+    total_price = 0
 
-#     for item in stock:
-#         if item in prices:
-#             total_price += stock[item] * prices[item]
+    for item in stock:
+        if item in prices:
+            total_price += stock[item] * prices[item]
     
-#     return total_price
+    return total_price
 
-# stock = {
-#     "banana": 6,
-#     "apple": 0,
-#     "orange": 32,
-#     "pear": 15
-# }
-# prices = {
-#     "banana": 4,
-#     "apple": 2,
-#     "orange": 1.5,
-#     "pear": 3
-# }
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
 
-# total_price = calcul_total(stock, prices)
-# print(f"The total price of the stock is: ${total_price}")
+total_price = calcul_total(stock, prices)
+print(f"The total price of the stock is: ${total_price}")
 
 # task 3
-lista = []
-for i in range(1, 10):
-    lista.append(i)
-    squer = []
-    for j in squer:
-        squer.append(j**2)
-print(lista)
+result = [(i, i ** 2) for i in range(1, 11)]
+print(result)
