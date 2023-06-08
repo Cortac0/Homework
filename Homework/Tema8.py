87c51ff4e508cf99a1128706581e90256ee5865f
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-06-08 19:11:50 +0300

    Corectat

--- Homework/Tema8.py
+++ Homework/Tema8.py
@@ -1,29 +1,36 @@
 # task 1
-def favorite_movie(movie):
-    print(f'Filmul meu preferat se numeste "{movie}"')
-favorite_movie('Prey')
+# def favorite_movie(movie):
+#     print(f'Filmul meu preferat se numeste "{movie}"')
+# favorite_movie('Prey')
 
 # task 2
-def test2(d: dict) -> str: 
-    for k, v in d.items():
-        print(f'Tara {k} are capitala {v}')
+# def test2(d: dict) -> str: 
+#     for k, v in d.items():
+#         print(f'Tara {k} are capitala {v}')
         
 
-dictionar = {
-    'Moldova': 'Chisinau',
-    'Romania': 'Bucuresti',
-    'Italia': 'Roma',
-}
+# dictionar = {
+#     'Moldova': 'Chisinau',
+#     'Romania': 'Bucuresti',
+#     'Italia': 'Roma',
+# }
  
-if __name__ == '__main__':
-    rezultat = test2(dictionar)
-    print(rezultat)
+# if __name__ == '__main__':
+#     rezultat = test2(dictionar)
+#     print(rezultat)
 
 # task 3
-def make_operation(*args):
+def make_operation(operation, *args):
     total = 0
+    operation = args.pop(0)
+    
     for num in args:
-        total += num
+        if operation == '-':
+            total -= num
+        elif operation == '+':
+            total += num
+    else:
+        total * num
     return total
 
 print(make_operation(7, 7, 2))  
