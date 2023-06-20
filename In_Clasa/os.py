256c2634a15a20b543dfb7bb31c62b5e39671581
Cortac0 <134640911+Cortac0@users.noreply.github.com>
2023-06-20 21:16:01 +0300

    Rez

--- In_Clasa/os.py
+++ In_Clasa/os.py
@@ -1,7 +1,11 @@
-import os
 
-x = 0
 
-print(os.name) # arata ce de pe ce sistem de operare se executa codul 
-print(os.getcwd()) # arata traseul de unde se executa programul
-print(dir()) # arata tot ce a importat sau ce a folosit programul curent
+# x = 0
+
+# print(os.name) # arata ce de pe ce sistem de operare se executa codul 
+# print(os.getcwd()) # arata traseul de unde se executa programul
+# print(dir()) # arata tot ce a importat sau ce a folosit programul curent
+
+x = input("just hit enter")
+print('=' * 20)
+print(type(x))
